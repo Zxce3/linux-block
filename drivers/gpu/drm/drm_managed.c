c07ff8dcbb8d5f45bc4dce00056f908b0668b4ce
Catalin Marinas <catalin.marinas@arm.com>
2022-03-27 18:41:17 +0100

    drivers/gpu: Use ARCH_DMA_MINALIGN instead of ARCH_KMALLOC_MINALIGN
    ARCH_DMA_MINALIGN represents the minimum (static) alignment for safe DMA
    operations while ARCH_KMALLOC_MINALIGN is the minimum kmalloc() objects
    alignment.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Daniel Vetter <daniel@ffwll.ch>

--- drivers/gpu/drm/drm_managed.c
+++ drivers/gpu/drm/drm_managed.c
@@ -48,10 +48,10 @@ struct drmres {
 	 * Some archs want to perform DMA into kmalloc caches
 	 * and need a guaranteed alignment larger than
 	 * the alignment of a 64-bit integer.
-	 * Thus we use ARCH_KMALLOC_MINALIGN here and get exactly the same
+	 * Thus we use ARCH_DMA_MINALIGN here and get at least the same
 	 * buffer alignment as if it was allocated by plain kmalloc().
 	 */
-	u8 __aligned(ARCH_KMALLOC_MINALIGN) data[];
+	u8 __aligned(ARCH_DMA_MINALIGN) data[];
 };
 
 static void free_dr(struct drmres *dr)
