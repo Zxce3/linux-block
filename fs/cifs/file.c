ccc1acdfa787b5bf5c9c2b3f829b18f5a0bb0938
David Howells <dhowells@redhat.com>
2022-03-29 15:25:55 +0100

    mm, netfs, fscache: Stop read optimisation when folio removed from pagecache
    Fscache has an optimisation by which reads from the cache are skipped until
    we know that (a) there's data there to be read and (b) that data isn't
    entirely covered by pages resident in the netfs pagecache.  This is done
    with two flags manipulated by fscache_note_page_release():
    
    	if (...
    	    test_bit(FSCACHE_COOKIE_HAVE_DATA, &cookie->flags) &&
    	    test_bit(FSCACHE_COOKIE_NO_DATA_TO_READ, &cookie->flags))
    		clear_bit(FSCACHE_COOKIE_NO_DATA_TO_READ, &cookie->flags);
    
    where the NO_DATA_TO_READ flag causes cachefiles_prepare_read() to indicate
    that netfslib should download from the server or clear the page instead.
    
    The fscache_note_page_release() function is intended to be called from
    ->releasepage() - but that only gets called if PG_private or PG_private_2
    is set - and currently the former is at the discretion of the netfs and the
    latter is only set whilst a page is being written to the cache, so
    sometimes we miss clearing the optimisation.
    
    Fix this by adding an extra address_space operation, ->removing folio(),
    and flag, AS_NOTIFY_REMOVING_FOLIO.  The operation is called if the flag is
    set when a folio is removed from the pagecache.  The flag should be set if
    a non-NULL cookie is obtained from fscache and cleared in ->evict_inode()
    before truncate_inode_pages_final() is called.
    
    This can be tested by reading some data and then dropping caches.  The
    number of times the new op is called is counted in /proc/fs/fscache/stats:
    
    	RdHelp : DR=0 RA=4100 RP=0 WB=0 WBZ=0 rm=131072 <----
    
    Reported-by: Rohith Surabattula <rohiths.msft@gmail.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: Matthew Wilcox <willy@infradead.org>
    cc: Steve French <sfrench@samba.org>
    cc: Shyam Prasad N <nspmangalore@gmail.com>
    cc: Rohith Surabattula <rohiths.msft@gmail.com>
    cc: linux-cifs@vger.kernel.org

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -4643,6 +4643,7 @@ const struct address_space_operations cifs_addr_ops = {
 	.direct_IO = cifs_direct_io,
 	.invalidate_folio = cifs_invalidate_folio,
 	.launder_folio = cifs_launder_folio,
+	.removing_folio	= netfs_removing_folio,
 	/*
 	 * TODO: investigate and if useful we could add an cifs_migratePage
 	 * helper (under an CONFIG_MIGRATION) in the future, and also
